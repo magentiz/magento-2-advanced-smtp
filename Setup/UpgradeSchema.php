98f57cfebf4fd444d534a2ed36d2a1ead6655db0
vuong <vuong@onlinebizsoft.com>
2023-11-10 10:42:14 +0700

    update add smtp log columns

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -24,7 +24,26 @@ class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
         $connection = $installer->getConnection();
 
         if ($installer->tableExists('mageplaza_smtp_log')) {
-            if(! $connection->tableColumnExists('mageplaza_smtp_log', 'error_message')) {
+            if (! $connection->tableColumnExists('mageplaza_smtp_log', 'is_save_attachment')) {
+                $connection->addColumn($setup->getTable('mageplaza_smtp_log'), 'is_save_attachment', [
+                    'type'     => Table::TYPE_SMALLINT,
+                    'nullable' => false,
+                    'default'  => 0,
+                    'length'   => null,
+                    'comment'  => 'Is Save Attachment'
+                ]);
+            }
+
+            if (! $connection->tableColumnExists('mageplaza_smtp_log', 'attachment')) {
+                $connection->addColumn($setup->getTable('mageplaza_smtp_log'), 'attachment', [
+                    'type'     => Table::TYPE_TEXT,
+                    'nullable' => true,
+                    'length'   => null,
+                    'comment'  => 'Attachment'
+                ]);
+            }
+
+            if (! $connection->tableColumnExists('mageplaza_smtp_log', 'error_message')) {
                 $connection->addColumn($setup->getTable('mageplaza_smtp_log'), 'error_message', [
                     'type'     => Table::TYPE_TEXT,
                     'nullable' => true,
@@ -33,7 +52,7 @@ class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
                 ]);
             }
 
-            if(! $connection->tableColumnExists('mageplaza_smtp_log', 'email_template')) {
+            if (! $connection->tableColumnExists('mageplaza_smtp_log', 'email_template')) {
                 $connection->addColumn($setup->getTable('mageplaza_smtp_log'), 'email_template', [
                     'type'     => Table::TYPE_TEXT,
                     'nullable' => true,
